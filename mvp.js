395fa33840745b1fc12dbf90466cbb373008727c
FloValdes <ftvaldes1@uc.cl>
2020-11-28 18:51:47 -0300

    force push

--- mvp.js
+++ mvp.js
@@ -1,9 +1,63 @@
-const g = d3.select('#svg-canvas')
+const scale = d3.scalePow()
+          .exponent(2)
+          .domain([0, 350]) // velocity (m/s)
+          .range([0, 200]) // pixeles of length
+
+const speedScale = d3.scaleLinear()
+              .domain([1, 350])
+              .range([15000, 1050])
+
+let SPEED = speedScale(50);
+let FLUIDSPEED = 50;
+let GAS = "aire";
+let X_DYNAMIC = 228;
+
+const gasColor = {
+  "aire": {color: "lightblue", particles: "blue", rho: 1.205},
+  "co2": {color: "#4a4a4a", particles: "#242424", rho: 1.842},
+  "benceno": {color: "#abe0a4", particles: "#71d964", rho: 3.486},
+  "metano": {color: "#fcdcfc", particles: "#ffb8ff", rho: 0.668},
+  "ozono": {color: "#7474c4", particles: "#4848c7", rho: 2.14},
+  "dicloro": {color: "#d9e092", particles: "#cfe02b", rho: 2.9941},
+  "vapor": {color: "#dbdbdb", particles: "#a1a1a1", rho: 0.804},
+}
+
+// create different g for different z of elements
+const g0 = d3.select('#svg-canvas')
+                  .append('g')
+                  .attr('transform', 'translate(250, 500)');
+
+const g1 = d3.select('#svg-canvas')
             .append('g')
-            .attr('transform', 'translate(250, 400)')
+            .attr('transform', 'translate(250, 500)');
+
+const g2 = d3.select('#svg-canvas')
+            .append('g')
+            .attr('transform', 'translate(250, 500)');
+
+function updateFluid(previousHeight) {
+  g1.selectAll('circle')
+    .remove()
+    
+  if (previousHeight) {
+    d3.select("#svg-canvas")
+      .select("#static-pressure-fluid")
+      .attr("height", 150*(gasColor["aire"].rho / gasColor[GAS].rho));
+
+    d3.select("#svg-canvas")
+      .select("#dynamic-tube-static-pressure")
+      .attr("height", 150*(gasColor["aire"].rho / gasColor[GAS].rho));
+
+    d3.select('#dynamic-tube-dynamic-pressure')
+      .attr('x', X_DYNAMIC - (previousHeight - 150*(gasColor["aire"].rho / gasColor[GAS].rho)));
+
+    X_DYNAMIC = X_DYNAMIC - (previousHeight - 150*(gasColor["aire"].rho / gasColor[GAS].rho));
+
+  }
+}
 
 // Create pipe
-g.append('rect')
+g0.append('rect')
   .attr('width', 800)
   .attr('height', 300)
   .attr('fill', 'silver')
@@ -11,47 +65,73 @@ g.append('rect')
   .attr('y', -150)
 
 // fluid inside pipe
-g.append('rect')
+g0.append('rect')
   .attr('width', 800)
-  .attr('height', 200)
+  .attr('height', 250)
   .attr('fill', 'lightblue')
   .attr('x', -400)
   .attr('y', -125)
+  .attr('class', 'liquid')
+
+const scaleCounter = d3.scaleLinear()
+                .domain([0, 350])
+                .range([500, 100])
+
+var counter = 500;
+var createFluidDrop = function(){
+    g1.append('circle')
+      .attr('r', 5)
+      .attr('cx', -255)
+      .attr('cy', Math.floor(Math.random() * (100 - -120) + -120))
+      .attr('fill', gasColor[GAS].particles)
+      .transition()
+      .duration(SPEED)
+      .ease(d3.easeLinear)
+      .attr('cx', 400)
+      .remove()
+
+    counter = Math.floor(scaleCounter(FLUIDSPEED));
+    // console.log(counter);
+    timeout = setTimeout(createFluidDrop, counter);
+}
+var timeout = setTimeout(createFluidDrop, counter);
 
 // static pressure tube
-g.append('rect')
+g2.append('rect')
   .attr('width', 50)
-  .attr('height', 200)
+  .attr('height', 400)
   .attr('x', -100)
-  .attr('y', -280)
+  .attr('y', -480)
   .attr('fill', 'silver')
 
-g.append('rect')
+g2.append('rect')
   .attr('width', 30)
-  .attr('height', 125)
+  .attr('height', 424)
   .attr('x', -90)
-  .attr('y', -320)
+  .attr('y', -520)
   .attr('fill', 'white')
 
-g.append('rect')
+g2.append('rect')
   .attr('width', 30)
-  .attr('height', 125)
-  .attr('x', -90)
-  .attr('y', -200)
+  .attr('height', 150)
+  .attr('transform', 'rotate(180)')
+  .attr('x', 60)
+  .attr('y', 80)
   .attr('fill', 'lightblue')
-
+  .attr('class', 'liquid')
+  .attr('id', 'static-pressure-fluid')
 
 // dynamic pressure tube
 
 // pipe
-g.append('rect')
+g2.append('rect')
   .attr('width', 50)
-  .attr('height', 400)
+  .attr('height', 500)
   .attr('x', 200)
-  .attr('y', -405)
+  .attr('y', -505)
   .attr('fill', 'silver')
 
-g.append('rect')
+g2.append('rect')
   .attr('width', 175)
   .attr('height', 50)
   .attr('x', 75)
@@ -59,74 +139,115 @@ g.append('rect')
   .attr('fill', 'silver')
 
 // pipe's white background 
-g.append('rect')
+g2.append('rect')
   .attr('width', 30)
-  .attr('height', 370)
+  .attr('height', 470)
   .attr('x', 210)
-  .attr('y', -410)
+  .attr('y', -510)
   .attr('fill', 'white')
 
 // vertical fluid
-g.append('rect')
+g2.append('rect')
   .attr('width', 30)
-  .attr('height', 175)
+  .attr('height', 55)
   .attr('x', 210)
-  .attr('y', -200)
+  .attr('y', -80)
   .attr('fill', 'lightblue')
+  .attr('class', 'liquid')
+
+g2.append('rect')
+  .attr('width', 30)
+  .attr('height', 150)
+  .attr('x', -240)
+  .attr('y', 79)
+  .attr('fill', 'lightblue')
+  .attr('class', 'liquid')
+  .attr('transform', 'rotate(180)')
+  .attr('id', 'dynamic-tube-static-pressure')
 
 // horizontal fluid
-g.append('rect')
+g2.append('rect')
   .attr('width', 170)
   .attr('height', 30)
   .attr('x', 70)
   .attr('y', -40)
   .attr('fill', 'lightblue')
+  .attr('class', 'liquid')
 
 // dynamic pressure fluid
-g.append('rect')
+g2.append('rect')
   .attr('transform', 'rotate(270)')
   .attr('width', 29)
   .attr('height', 30)
-  .attr('x', 199)
+  .attr('x', 228)
   .attr('y', 210)
   .attr('fill', 'lightblue')
-  .attr('id', 'dynamicPressure')
+  .attr('class', 'liquid')
+  .attr('id', 'dynamic-tube-dynamic-pressure')
 
-// arrow
-g.append('rect')
-  .attr('width', 100)
-  .attr('height', 10)
-  .attr('x', -100)
-  .attr('y', -30)
-  .attr('fill', 'black')
+// pressure points
+g2.append('circle')
+  .attr('cx', -75)
+  .attr('cy', -25)
+  .attr('r', 10)
+  .attr('fill', 'red')
+  
+g2.append('text')
+  .text('1')
+  .attr('font-size', '16px')
+  .attr('font-family', 'sans-serif')
+  .attr('x', -80)
+  .attr('y', -20)
+  .attr('fill', 'white')
+  .attr('font-weight', 'bold');
 
-g.append('rect')
-  .attr('width', 35)
-  .attr('height', 10)
-  .attr('x', -50)
-  .attr('y', -27)
-  .attr('fill', 'black')
-  .attr('transform', 'rotate(30)')
+g2.append('circle')
+  .attr('cx', 65)
+  .attr('cy', -25)
+  .attr('r', 10)
+  .attr('fill', 'red')
+  
+g2.append('text')
+  .text('2')
+  .attr('font-size', '16px')
+  .attr('font-family', 'sans-serif')
+  .attr('x', 60)
+  .attr('y', -20)
+  .attr('fill', 'white')
+  .attr('font-weight', 'bold');
 
-g.append('rect')
-  .attr('width', 35)
-  .attr('height', 10)
-  .attr('x', -25)
-  .attr('y', -27)
-  .attr('fill', 'black')
-  .attr('transform', 'rotate(-30)')
 
-scale = d3.scalePow()
-          .exponent(2)
-          .domain([0, 350]) // velocity (m/s)
-          .range([0, 200]) // pixeles of length
 
 const slider = document.getElementById("myRange");
 const output = document.getElementById("value");
 
 slider.oninput = function() {
-  output.innerHTML = `Valor actual: ${this.value} m/s`;
-  d3.select('#dynamicPressure').attr('width', Math.round(scale(this.value,2)))
+  output.innerHTML = `Valor actual: ${this.value}`;
+  d3.select('#dynamic-tube-dynamic-pressure').attr('width', Math.round(scale(this.value)));
+  SPEED = speedScale(Math.round(this.value));
+  if (Math.round(this.value) === 0) {
+    SPEED = null;
+  }
+  FLUIDSPEED = this.value;
+  updateFluid();
+}
+
+// Agregar distintos gases
+const gasSelect = document.getElementById("gas-selector");
+
+gasSelect.onchange = function() {
+  // Change color
+  d3.select("#svg-canvas")
+    .selectAll(".liquid")
+    .attr("fill", gasColor[this.value].color);
+
+  // Change rho
+  document.getElementById("rho-value").innerHTML = gasColor[this.value].rho;
+
+  // get previous height
+  const previousHeight = 150*gasColor["aire"].rho / gasColor[GAS].rho;
+  GAS = this.value;
+  updateFluid(previousHeight);
 }
 
 
